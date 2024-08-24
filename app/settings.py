aa2c0ee33ecefed8de1ca996ca927752dca2307c
Jeffersson Barros <146139830+Jeffersson17@users.noreply.github.com>
2024-08-24 12:26:42 -0300

    alterando banco para postgres (#1)
    * excluindo banco sqlite
    
    * alterando configuracoes database no arquivo settings.py
    
    * excluindo arquivos de lixo

--- app/settings.py
+++ app/settings.py
@@ -77,11 +77,15 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'my_aplication',
+        'USER': 'postgres',
+        'PASSWORD': '120206',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
+ 
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
