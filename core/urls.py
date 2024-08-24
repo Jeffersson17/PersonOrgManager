d7c219b8fb803e9e6e092648b1afb699bc8b5c28
Jeffersson <barrosjefferson407@gmail.com>
2024-08-24 19:48:11 -0300

    Removido e adicionado opcoes nas urls

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
-from .views import home, PessoaListView, PessoaCreateView, PessoaUpdateView, PessoaDeleteView
+from core.views import PessoaListView, PessoaCreateView, PessoaUpdateView, PessoaDeleteView
 
 urlpatterns = [
-    path('', home),
-    path('pessoa/', PessoaListView.as_view(), name="pessoa_list"),
+    path('pessoa/', PessoaListView.as_view(), name="list"),
     path('create/', PessoaCreateView.as_view(), name="create"),
-    path('editar/<int:pk>/', PessoaUpdateView.as_view(), name='editar'),
+    path('editar/<int:pk>/', PessoaUpdateView.as_view(), name='update'),
     path('delete/<int:pk>/', PessoaDeleteView.as_view(), name='delete'),
 ]
