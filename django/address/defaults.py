09cdcf3dd9b12bd0ce4f5a8c716e7d5fd80f02f0
Jeffersson <barrosjefferson407@gmail.com>
2024-09-09 14:56:29 -0300

    Criação do form e do defaults.py onde esta os choices

--- django/address/defaults.py
+++ django/address/defaults.py
@@ -41,31 +41,62 @@ SAO_PAULO = 'SP'
 SERGIPE = 'SE'
 TOCANTINS = 'TO'
 STATE_CITY_CHOICE = {
-    ACRE: 'AC',
-    ALAGOAS: 'AL',
-    AMAPA: 'AP',
-    AMAZONAS: 'AM',
-    BAHIA: 'BA',
-    CEARA: 'CE',
-    DISTRITO_FEDERAL: 'DF',
-    ESPIRITO_SANTO: 'ES',
-    GOIAS: 'GO',
-    MARANHAO: 'MA',
-    MATO_GROSSO: 'MT',
-    MATO_GROSSO_DO_SUL: 'MS',
-    MINAS_GERAIS: 'MG',
-    PARA: 'PA',
-    PARAIBA: 'PB',
-    PARANA: 'PR',
-    PERNAMBUCO: 'PE',
-    PIAUI: 'PI',
-    RIO_DE_JANEIRO: 'RJ',
-    RIO_GRANDE_DO_NORTE: 'RN',
-    RIO_GRANDE_DO_SUL: 'RS',
-    RONDONIA: 'RO',
-    RORAIMA: 'RR',
-    SANTA_CATARINA: 'SC',
-    SAO_PAULO: 'SP',
-    SERGIPE: 'SE',
-    TOCANTINS: 'TO',
+    ACRE: 'Acre',
+    ALAGOAS: 'Alagoas',
+    AMAPA: 'Amapá',
+    AMAZONAS: 'Amazonas',
+    BAHIA: 'Bahia',
+    CEARA: 'Ceará',
+    DISTRITO_FEDERAL: 'Distrito Federal',
+    ESPIRITO_SANTO: 'Espírito Santo',
+    GOIAS: 'Goiás',
+    MARANHAO: 'Maranhão',
+    MATO_GROSSO: 'Mato Grosso',
+    MATO_GROSSO_DO_SUL: 'Mato Grosso do Sul',
+    MINAS_GERAIS: 'Minas Gerais',
+    PARA: 'Pará',
+    PARAIBA: 'Paraíba',
+    PARANA: 'Paraná',
+    PERNAMBUCO: 'Pernambuco',
+    PIAUI: 'Piauí',
+    RIO_DE_JANEIRO: 'Rio de Janeiro',
+    RIO_GRANDE_DO_NORTE: 'Rio Grande do Norte',
+    RIO_GRANDE_DO_SUL: 'Rio Grande do Sul',
+    RONDONIA: 'Rondônia',
+    RORAIMA: 'Roraima',
+    SANTA_CATARINA: 'Santa Catarina',
+    SAO_PAULO: 'São Paulo',
+    SERGIPE: 'Sergipe',
+    TOCANTINS: 'Tocantins',
+}
+
+SAUDE =  'SD'
+TECNOLOGIA_DA_INFORMACAO = 'TI'
+SERVICOS_GERAIS = 'SG'
+FINANCEIRO = 'FC'
+VENDAS = 'VD'
+JURIDICO = 'JD'
+PESQUISA_E_DESENVOLVIMENTO = 'PD'
+ADMINISTACAO = 'AD'
+MARKETING = 'MK'
+RECURSOS_HUMANOS = 'RH'
+COMUNICACAO_INTERNA = 'CI'
+DESIGN = 'DS'
+E_COMMERCE = 'EC'
+PLANEJAMENTO_E_ANALISE_DE_NEGOCIOS = 'PAN'
+AREA_CHOICES = {
+    SAUDE: 'Saúde',
+    TECNOLOGIA_DA_INFORMACAO: 'Tecnologia da Informação',
+    SERVICOS_GERAIS: 'Serviços Gerais',
+    FINANCEIRO: 'Financeiro',
+    VENDAS: 'Vendas',
+    JURIDICO: 'Jurídico',
+    PESQUISA_E_DESENVOLVIMENTO: 'Pesquisa e Desenvolvimento',
+    ADMINISTACAO: 'Administração',
+    MARKETING: 'Marketing',
+    RECURSOS_HUMANOS: 'Recursos Humanos',
+    COMUNICACAO_INTERNA: 'Comunicação Interna',
+    DESIGN: 'Design',
+    E_COMMERCE: 'E-commerce',
+    PLANEJAMENTO_E_ANALISE_DE_NEGOCIOS: 'Planejamento e Análise de Negócios',
 }
