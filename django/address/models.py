662231fac464bd62f421fcdbebc1778fee022578
Jeffersson Barros <146139830+Jeffersson17@users.noreply.github.com>
2024-09-15 21:02:30 -0300

    New app (#8)
    * Criação do app organization, e do model Organization
    
    * Criação do app address, dos models City e Address e do arquivo defaults.py
    
    * Novos apps adicionados nas settings
    
    * VIew do app organization
    
    * verbose_name no model Organization

---
+++ django/address/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+from defaults import ADDRESS_TYPE, STATE_CITY_CHOICE
+
+
+class City(models.Model):
+    name = models.CharField(max_length=20)
+    state = models.CharField(STATE_CITY_CHOICE, max_length=2)
+
+
+    class Meta:
+        verbose_name = 'Cidade'
+        verbose_name_plural = 'Cidades'
+        ordering = ['name']
+
+
+class Address(models.Model):
+    address = models.CharField(max_length=50)
+    address_type = models.CharField(ADDRESS_TYPE, max_length=2)
+    city = models.ForeignKey(City, on_delete=models.CASCADE)
+    cep = models.CharField(max_length=8)
+    number = models.CharField(max_length=15)
+    complement = models.CharField(max_length=250, blank=True, null=True)
+
+
+    class Meta:
+        verbose_name = 'Endereço'
+        verbose_name_plural = 'Endereços'
+        ordering = ['name']
---
+++ django/organization/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from address.models import Address
+
+# Create your models here.
+class Organization(models.Model):
+    name = models.CharField(max_length=10)
+    phone = models.CharField(max_length=20)
+    address = models.ForeignKey(Address, unique=True, on_delete=models.SET_NULL)
+    area = models.CharField()
+    description = models.CharField(max_length=250)
+
+
+    class Meta:
+        verbose_name = 'Organização'
+        verbose_name_plural = 'Organizações'
