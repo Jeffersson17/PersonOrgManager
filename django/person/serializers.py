8b2f09fdeba6244f49d0db78d23c1d992978722f
Jeffersson <barrosjefferson407@gmail.com>
2024-11-08 18:47:50 -0300

    Adição de validators

--- django/person/serializers.py
+++ django/person/serializers.py
@@ -2,8 +2,19 @@ from .models import Pessoa
 from rest_framework import serializers
 
 
-
 class PessoaSerializer(serializers.ModelSerializer):
     class Meta:
         model = Pessoa
         fields = ['id', 'nome', 'idade']
+
+
+    def validate_idade(self, value):
+        if value < 18:
+            raise serializers.ValidationError('A idade não pode ser menor que 18.')
+        return value
+
+
+    def validate_nome(self, value):
+        if len(value) <= 3:
+            raise serializers.ValidationError('O nome deve conter mais de 3 caracteres.')
+        return value
